e41c064a1d7e005d97d67fde41a38ef0fb662311
xDangerousDinox <ahoyt2014@gmail.com>
2018-01-23 15:44:18 -0800

    test

--- src/org/usfirst/frc/team949/robot/subsystems/TakeIn_PutOut.java
+++ src/org/usfirst/frc/team949/robot/subsystems/TakeIn_PutOut.java
@@ -27,7 +27,7 @@ public class TakeIn_PutOut extends Subsystem {
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here. 
+        // Set the default command for a subsystem here.  
         //setDefaultCommand(new MySpecialCommand());
     }
 }
