8f08319652a49c2d80b8c22b8412742c0a14364c
eyoon <eyoon@10.104.41.24>
2018-01-27 15:52:35 -0800

    End of Week 4 Day 1.

--- src/org/usfirst/frc/team949/robot/commands/JoyStickDrive.java
+++ src/org/usfirst/frc/team949/robot/commands/JoyStickDrive.java
@@ -5,10 +5,12 @@ import org.usfirst.frc.team949.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *
+ * Teleop command to drive the robot via Joystick.
  */
 public class JoyStickDrive extends Command {
 	
+	private final static double Y_THRESHOLD = 0.5;
+	private final static double Z_THRESHOLD = 0.5;
 	
     public JoyStickDrive() {
     	requires(Robot.driveTrain);
@@ -21,7 +23,7 @@ public class JoyStickDrive extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	
-    	Robot.driveTrain.drive(Robot.oi.getDriveX(), Robot.oi.getDriveZ());
+    	Robot.driveTrain.drive(Robot.oi.getDriveY(), Robot.oi.getDriveZ());
     }
 
     // Make this return true when this Command no longer needs to run execute()
